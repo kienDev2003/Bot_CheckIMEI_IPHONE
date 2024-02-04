1c606b3b23dbb08c1d75a9c24cc62e00e0921739
kienDev2003 <kiengsm03@gmail.com>
2024-02-04 23:17:20 +0700

    upload code check imei iphone

---
+++ CheckIMEI/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CheckIMEI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessage.Location = new System.Drawing.Point(0, 0);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(780, 527);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 527);
+            this.Controls.Add(this.txtMessage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ CheckIMEI/Form1.cs
@@ -0,0 +1,220 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace CheckIMEI
+{
+    public partial class Form1 : Form
+    {
+        TelegramBotClient botClient = null;
+        int process = 0;
+        string data = "Không lấy được data";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenBot();
+        }
+
+        private void OpenBot()
+        {
+            string tokenBot = ConfigurationManager.AppSettings["tokenBot"].ToString();
+            if (botClient == null)
+            {
+                botClient = new TelegramBotClient(tokenBot);
+            }
+            botClient.StartReceiving();
+            botClient.OnMessage += BotClient_OnMessage;
+        }
+
+        private void BotClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            string chatID = e.Message.Chat.Id.ToString();
+            string message = e.Message.Text;
+
+            if (message.StartsWith("/check "))
+            {
+                string imei = message.Substring("/check ".Length);
+                botClient.SendTextMessageAsync(chatID, "Đang truy vấn. Vui lòng chờ !");
+                if (process == 0)
+                {
+                    string data = CheckInfoImei(imei);
+                    botClient.SendTextMessageAsync(chatID, data);
+                }
+                else
+                {
+                    botClient.SendTextMessageAsync(chatID, "Hệ thống bận. Thử lại sau 3 giây !");
+                }
+            }
+        }
+
+        private string CheckInfoImei(string imei)
+        {
+            bool start = true;
+            IWebDriver chrome = null;
+            do
+            {
+                try
+                {
+                    process = 1;
+
+                    string url = ConfigurationManager.AppSettings["url_Check"].ToString();
+
+                    ////List<ProxyInfo> proxyInfos = GetIPAndPort();
+                    //string ip = "202.131.65.110";//proxyInfos[0].IP.ToString();
+                    //string port = "80";//proxyInfos[0].Port.ToString();
+
+                    ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService();
+                    chromeService.HideCommandPromptWindow = true;
+
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 120.0.0.0 Safari / 537.36");
+                    chromeOptions.AddArgument("disable-blink-features=AutomationControlled");
+                    chromeOptions.AddArgument("--disable-javascript");
+                    //chromeOptions.AddArgument($"--proxy-server=https://{ip}:{port}");
+                    //chromeOptions.AddArgument("--headless");
+
+                    chrome = new ChromeDriver(chromeService, chromeOptions);
+
+                    bool goUrl = true;
+                    do
+                    {
+                        try
+                        {
+                            //proxyInfos = GetIPAndPort();
+                            //ip = proxyInfos[0].IP.ToString();
+                            //port = proxyInfos[0].Port.ToString();
+
+                            chrome.Navigate().GoToUrl(url);
+                            goUrl = false;
+                        }
+                        catch (Exception e)
+                        {
+                            goUrl = true;
+                        }
+                    }
+                    while (goUrl);
+                    Thread.Sleep(1000);
+
+                    chrome.FindElement(By.Id("imei")).SendKeys(imei);
+
+                    IWebElement chromeElement = chrome.FindElement(By.Id("service"));
+                    SelectElement select = new SelectElement(chromeElement);
+                    select.SelectByValue("30");
+                    Thread.Sleep(2000);
+
+                    chrome.FindElement(By.Id("submit")).Click();
+
+                    bool check = true;
+                    int count = 0;
+                    do
+                    {
+                        if (count == 1000)
+                        {
+                            chrome.Quit();
+
+                            process = 0;
+
+                            return data;
+                        }
+                        try
+                        {
+                            if (chrome.FindElement(By.Id("copy")) != null)
+                            {
+                                check = false;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            count++;
+                            check = true;
+                        }
+
+                    }
+                    while (check);
+
+                    chrome.FindElement(By.Id("copy")).Click();
+                    Thread.Sleep(1000);
+
+                    this.Invoke(new Action(() =>
+                    {
+                        if (Clipboard.ContainsText())
+                        {
+                            data = Clipboard.GetText();
+                            data = data.Replace("SICKW.COM", "DONE");
+                        }
+                    }));
+
+                    chrome.Quit();
+                    start = false;
+                    process = 0;
+
+                }
+                catch (Exception e)
+                {
+                    if(chrome != null) chrome.Quit();
+                    start = true;
+                }
+            } while (start);
+
+            return data;
+        }
+
+        class ProxyInfo
+        {
+            public string IP { get; set; }
+
+            public string Port { get; set; }
+        }
+
+        private List<ProxyInfo> GetIPAndPort()
+        {
+            List<ProxyInfo> list = new List<ProxyInfo>();
+            string url_Proxy = ConfigurationManager.AppSettings["url_Proxy"].ToString();
+
+            ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService();
+            chromeService.HideCommandPromptWindow = true;
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+
+            IWebDriver chrome = new ChromeDriver(chromeService, chromeOptions);
+
+            chrome.Navigate().GoToUrl(url_Proxy);
+
+            string html_Proxy = chrome.PageSource;
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html_Proxy);
+
+            var table = doc.DocumentNode.Descendants("table").Where(_table => _table.Attributes.Contains("class") && _table.Attributes["class"].Value == "table table-striped table-bordered").FirstOrDefault();
+            var tbody = table.Descendants("tbody").FirstOrDefault();
+            var tr = tbody.Descendants("tr").ToList();
+            int ramdom = new Random().Next(1,19);
+            var td = tr[ramdom].Descendants("td").ToList();
+
+            list.Add(new ProxyInfo { IP = td.ElementAt(0).InnerText, Port = td.ElementAt(1).InnerText });
+
+            chrome.Quit();
+
+            return list;
+        }
+    }
+}
