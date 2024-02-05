6b4e0048ad09c59d9abd936aac50088fda925fd3
kienDev2003 <kiengsm03@gmail.com>
2024-02-05 22:35:07 +0700

    update mode and fix bug

--- CheckIMEI/Form1.cs
+++ CheckIMEI/Form1.cs
@@ -49,13 +49,41 @@ namespace CheckIMEI
             string chatID = e.Message.Chat.Id.ToString();
             string message = e.Message.Text;
 
-            if (message.StartsWith("/check "))
+            if (message.StartsWith("/infoBasic "))
             {
-                string imei = message.Substring("/check ".Length);
+                string imei = message.Substring("/infoBasic ".Length);
                 botClient.SendTextMessageAsync(chatID, "Đang truy vấn. Vui lòng chờ !");
                 if (process == 0)
                 {
-                    string data = CheckInfoImei(imei);
+                    string data = CheckInfoImei(imei, 30);
+                    botClient.SendTextMessageAsync(chatID, data);
+                }
+                else
+                {
+                    botClient.SendTextMessageAsync(chatID, "Hệ thống bận. Thử lại sau 3 giây !");
+                }
+            }
+            else if (message.StartsWith("/lock "))
+            {
+                string imei = message.Substring("/lock ".Length);
+                botClient.SendTextMessageAsync(chatID, "Đang truy vấn. Vui lòng chờ !");
+                if (process == 0)
+                {
+                    string data = CheckInfoImei(imei, 103);
+                    botClient.SendTextMessageAsync(chatID, data);
+                }
+                else
+                {
+                    botClient.SendTextMessageAsync(chatID, "Hệ thống bận. Thử lại sau 3 giây !");
+                }
+            }
+            else if (message.StartsWith("/icloud "))
+            {
+                string imei = message.Substring("/icloud ".Length);
+                botClient.SendTextMessageAsync(chatID, "Đang truy vấn. Vui lòng chờ !");
+                if (process == 0)
+                {
+                    string data = CheckInfoImei(imei, 3);
                     botClient.SendTextMessageAsync(chatID, data);
                 }
                 else
@@ -65,7 +93,7 @@ namespace CheckIMEI
             }
         }
 
-        private string CheckInfoImei(string imei)
+        private string CheckInfoImei(string imei, int mode)
         {
             bool start = true;
             IWebDriver chrome = null;
@@ -77,19 +105,13 @@ namespace CheckIMEI
 
                     string url = ConfigurationManager.AppSettings["url_Check"].ToString();
 
-                    ////List<ProxyInfo> proxyInfos = GetIPAndPort();
-                    //string ip = "202.131.65.110";//proxyInfos[0].IP.ToString();
-                    //string port = "80";//proxyInfos[0].Port.ToString();
-
                     ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService();
                     chromeService.HideCommandPromptWindow = true;
 
                     ChromeOptions chromeOptions = new ChromeOptions();
-                    chromeOptions.AddArgument("Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 120.0.0.0 Safari / 537.36");
+                    chromeOptions.AddArgument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
                     chromeOptions.AddArgument("disable-blink-features=AutomationControlled");
                     chromeOptions.AddArgument("--disable-javascript");
-                    //chromeOptions.AddArgument($"--proxy-server=https://{ip}:{port}");
-                    //chromeOptions.AddArgument("--headless");
 
                     chrome = new ChromeDriver(chromeService, chromeOptions);
 
@@ -98,11 +120,7 @@ namespace CheckIMEI
                     {
                         try
                         {
-                            //proxyInfos = GetIPAndPort();
-                            //ip = proxyInfos[0].IP.ToString();
-                            //port = proxyInfos[0].Port.ToString();
-
-                            chrome.Navigate().GoToUrl(url);
+                            chrome.Navigate().GoToUrl("https://sickw.com");
                             goUrl = false;
                         }
                         catch (Exception e)
@@ -117,7 +135,7 @@ namespace CheckIMEI
 
                     IWebElement chromeElement = chrome.FindElement(By.Id("service"));
                     SelectElement select = new SelectElement(chromeElement);
-                    select.SelectByValue("30");
+                    select.SelectByValue(mode.ToString());
                     Thread.Sleep(2000);
 
                     chrome.FindElement(By.Id("submit")).Click();
@@ -169,52 +187,12 @@ namespace CheckIMEI
                 }
                 catch (Exception e)
                 {
-                    if(chrome != null) chrome.Quit();
+                    if (chrome != null) chrome.Quit();
                     start = true;
                 }
             } while (start);
 
             return data;
-        }
-
-        class ProxyInfo
-        {
-            public string IP { get; set; }
-
-            public string Port { get; set; }
-        }
-
-        private List<ProxyInfo> GetIPAndPort()
-        {
-            List<ProxyInfo> list = new List<ProxyInfo>();
-            string url_Proxy = ConfigurationManager.AppSettings["url_Proxy"].ToString();
-
-            ChromeDriverService chromeService = ChromeDriverService.CreateDefaultService();
-            chromeService.HideCommandPromptWindow = true;
-
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddArgument("--headless");
-
-            IWebDriver chrome = new ChromeDriver(chromeService, chromeOptions);
-
-            chrome.Navigate().GoToUrl(url_Proxy);
-
-            string html_Proxy = chrome.PageSource;
-
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html_Proxy);
-
-            var table = doc.DocumentNode.Descendants("table").Where(_table => _table.Attributes.Contains("class") && _table.Attributes["class"].Value == "table table-striped table-bordered").FirstOrDefault();
-            var tbody = table.Descendants("tbody").FirstOrDefault();
-            var tr = tbody.Descendants("tr").ToList();
-            int ramdom = new Random().Next(1,19);
-            var td = tr[ramdom].Descendants("td").ToList();
-
-            list.Add(new ProxyInfo { IP = td.ElementAt(0).InnerText, Port = td.ElementAt(1).InnerText });
-
-            chrome.Quit();
-
-            return list;
         }
     }
 }
